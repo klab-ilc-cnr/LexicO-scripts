67da55730b2239a71b8932872b2451820f3a4677
Simone Marchi <10340896+magoafono@users.noreply.github.com>
2023-12-15 16:33:06 +0100

    Add files via upload
    Charset updated

--- 01_Create_Redundant_Tables.sql
+++ 01_Create_Redundant_Tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE `RedundantPhu` (
   KEY `RedundantPHU_FK_1` (`idRedundantOf`),
   CONSTRAINT `RedundantPHU_FK` FOREIGN KEY (`idRedundant`) REFERENCES `phu` (`idPhu`) ON DELETE CASCADE,
   CONSTRAINT `RedundantPHU_FK_1` FOREIGN KEY (`idRedundantOf`) REFERENCES `phu` (`idPhu`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `RedundantUsem` (
@@ -24,7 +24,7 @@ CREATE TABLE `RedundantUsem` (
   KEY `RedundantUsem_FK_1` (`idRedundantOf`),
   CONSTRAINT `RedundantUsem_FK` FOREIGN KEY (`idRedundant`) REFERENCES `usem` (`idUsem`)  ON DELETE CASCADE,
   CONSTRAINT `RedundantUsem_FK_1` FOREIGN KEY (`idRedundantOf`) REFERENCES `usem` (`idUsem`)  ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `RedundantUsyn` (
@@ -37,7 +37,7 @@ CREATE TABLE `RedundantUsyn` (
   KEY `RedundantUsyn_UN_1` (`idRedundantOf`),
   CONSTRAINT `RedundantUsyn_FK` FOREIGN KEY (`idRedundant`) REFERENCES `usyns` (`idUsyn`)  ON DELETE CASCADE,
   CONSTRAINT `RedundantUsyn_FK_1` FOREIGN KEY (`idRedundantOf`) REFERENCES `usyns` (`idUsyn`)  ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `RedundantMus` (
@@ -50,4 +50,4 @@ CREATE TABLE `RedundantMus` (
   KEY `RedundantMus_FK_1` (`idRedundantOf`),
   CONSTRAINT `RedundantMus_FK` FOREIGN KEY (`idRedundant`) REFERENCES `mus` (`idMus`) ON DELETE CASCADE,
   CONSTRAINT `RedundantMus_FK_1` FOREIGN KEY (`idRedundantOf`) REFERENCES `mus` (`idMus`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
